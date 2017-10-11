dd6d74408d977f9be71c4230143a7e5b7919b50c
Jeremey <jeremey.duvall@automattic.com>
2017-10-11 07:40:58 -0600

    Update footer to include attribution and link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import CalendarView from './views/calendar';
 import AddItemView from './views/additem';
+import Heart from 'genericons-neue-react/icons/heart';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Blockable</h1>
+                <header className="blockable-header">
+                    <h1 className="blockable-title">Blockable</h1>
                 </header>
                 <div className="blockable">
                     <CalendarView />
                     <AddItemView />
                 </div>
+                <footer className="blockable-footer">
+                    <span>Made with <Heart fill='#F67280'/> by <a href="https://jeremey.blog">Jeremey</a> - <a href="https://github.com/jeremeylduvall/blockable">Find out more on GitHub</a></span>
+                </footer>
             </div>
         )
     }
